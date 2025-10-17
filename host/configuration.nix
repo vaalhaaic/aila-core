5fc50da28836859c5e7756c4c5059c62cbdebd3f
vaalhaaic <cm99.vip@gmail.com>
2025-10-17 11:11:17 +0800

    fix(host): point systemd-boot at /boot/efi

--- host/configuration.nix
+++ host/configuration.nix
@@ -8,6 +8,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   nixpkgs.config.allowUnfreePredicate = pkg:
     let
